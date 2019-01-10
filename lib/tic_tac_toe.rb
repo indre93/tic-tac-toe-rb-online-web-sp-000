d1f7404beef414aebc974439bf8ccc8743065d33
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-10 18:29:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,3 +58,22 @@ def turn(board)
 end
 
 
+def turn_count(board)
+  counter = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      counter += 1
+    end
+  end
+counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    "X"
+  else
+    "O"
+  end
+end
+
+
